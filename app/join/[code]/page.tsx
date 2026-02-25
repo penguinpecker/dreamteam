f4a170b33f7f9efff42a02d3b9312c57beee9221
penguinpecker <penguinpecker1@gmail.com>
2026-02-26 01:41:55 +0530

    fix: auto-open popup on Privy return + add /join/[code] redirect

---
+++ app/join/[code]/page.tsx
@@ -0,0 +1,6 @@
+import { redirect } from "next/navigation";
+
+export default async function JoinPage({ params }: { params: Promise<{ code: string }> }) {
+  const { code } = await params;
+  redirect(`/?ref=${code}`);
+}
